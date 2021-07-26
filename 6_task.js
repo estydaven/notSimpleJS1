a56fe169052345850b26f34216aa4a181a158e8e
estydaven <esetnod1111@gmail.com>
2021-07-26 11:44:18 +0300

    fix solutions of 6 and 8 tasks

--- 6_task.js
+++ 6_task.js
@@ -1,15 +1,12 @@
 //Задача 6. Нужно написать функцию, принимающую строку в качестве аргумента и возвращающую количество гласных, которые содержатся в строке. Гласными являются «a», «e», «i», «o», «u».
 
-let str = 'Mama myla ramu';
-let vowels = ['a', 'e', 'i', 'o', 'u'];
-let sum = 0;
-
-function getSumVowels (str) {
-    for (let i = 0; i < str.length; i++) {
-        for (let j = 0; j < vowels.length; j++) {
-            if (str[i] == vowels[j]) {
-                sum++;
-            }
+function getSumVowels (str) {  
+    let string = str.toLowerCase();
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let sum = 0; 
+    for (let item of string) {
+        if (vowels.includes(item)) {
+            sum++;
         }
     }
 
--- 8_task.js
+++ 8_task.js
@@ -3,6 +3,11 @@
 const arr = [1,2,3,4,5,6,7,8,9,10,];
 
 function getAvgArray (arr) {
+
+    if (arr.length === 0) {
+        return 'Массив пуст, посчитать среднее арифметическое невозможно'
+    }
+
     let sumArr = 0;
 
     for (let i = 0; i < arr.length; i++) {
